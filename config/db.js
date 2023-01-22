2e0f7b6752769a0f3491cd601f7940af05ccc4c7
dounya-m <mellouki.dounya@gmail.com>
2023-01-22 13:03:34 +0100

    error midlware

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const connDB =  async() => {
+    try{
+        const conn = await mongoose.connect(process.env.DB_URI)
+        console.log(`MongoDB are Connected: ${conn.connection.host}` .underline.yellow);
+    }catch(error){
+        console.log(error);
+        process.exit(1)
+    }
+} 
+module.exports = connDB
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 const colors = require('colors')
+const connDB = require('./config/db')
+const {erroHandler} = require('./middlewares/errorMiddleware')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+connDB()
 
+
+app.use(erroHandler)
 app.listen(PORT, () =>{
     console.log(`I'm Listen on PORT : ${PORT}`.underline.blue);
 })
